0f034c1238ae40c971e4a46e2cfb886176b36006
AKSHAY R <85187332+Zeus007-123@users.noreply.github.com>
2023-11-21 04:10:57 +0530

    Connect4Game.cpp

--- Connect4Game.cpp
+++ Connect4Game.cpp
@@ -1,120 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 
 using namespace std;
 
-class Connect4Game {
-private:
-    const int rows = 6;
-    const int columns = 7;
+class Board {
+protected:
+    static const int rows = 6;
+    static const int columns = 7;
+    static const int connectWin = 4;
     vector<vector<char>> board;
-    char currentPlayer;
-    bool gameWon;
 
 public:
-    Connect4Game() : currentPlayer('R'), gameWon(false) {
+    Board() {
         // Initialize the game board
         board.resize(rows, vector<char>(columns, ' '));
     }
 
-    // For Displaying the Game Board
+    virtual ~Board() {}
+
+    // Displaying the Game Board
     void displayBoard() const {
-      cout << "-----------------------------\n";
-      for (int i = 0; i < rows; ++i) {
-        cout << "| ";
-        for (int j = 0; j < columns; ++j) {
-            if (board[i][j] == 'R') {
-                cout << "\033[1;31mR \033[0m| ";
-            } else if (board[i][j] == 'B') {
-                cout << "\033[1;34mB \033[0m| ";
-            } else {
-                cout << board[i][j] << " | ";
-            }
-        }
-        cout << "\n-----------------------------\n";
-      }
-    }
-
-    // For Switching Players
-    void switchPlayer() {
-        currentPlayer = (currentPlayer == 'R') ? 'B' : 'R';
-    }
-
-    // For Checking if the Move is Valid or Not
-    bool isValidMove(int column) const {
-        return column >= 0 && column < columns && board[0][column] == ' ';
-    }
-
-    // For Making the move based Player Input
-    void makeMove(int column) {
-        for (int i = rows - 1; i >= 0; --i) {
-            if (board[i][column] == ' ') {
-                board[i][column] = currentPlayer;
-                break;
-            }
-        }
-    }
-
-    // For Checking if the Player has Won or not
-    bool checkWin() {
-        // Check horizontally, vertically, and diagonally for a win
-        return checkHorizontal() || checkVertical() || checkDiagonal();
-    }
-
-    // HORIZONTAL CHECK
-    bool checkHorizontal() const {
+        cout << "-----------------------------\n";
         for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j <= columns - 4; ++j) {
-                if (board[i][j] == currentPlayer &&
-                    board[i][j + 1] == currentPlayer &&
-                    board[i][j + 2] == currentPlayer &&
-                    board[i][j + 3] == currentPlayer) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    // VERTICAL CHECK
-    bool checkVertical() const {
-        for (int i = 0; i <= rows - 4; ++i) {
+            cout << "| ";
             for (int j = 0; j < columns; ++j) {
-                if (board[i][j] == currentPlayer &&
-                    board[i + 1][j] == currentPlayer &&
-                    board[i + 2][j] == currentPlayer &&
-                    board[i + 3][j] == currentPlayer) {
-                    return true;
+                if (board[i][j] == 'R') {
+                    cout << "\033[1;31mR \033[0m| ";
+                } else if (board[i][j] == 'B') {
+                    cout << "\033[1;34mB \033[0m| ";
+                } else {
+                    cout << board[i][j] << " | ";
                 }
             }
+            cout << "\n-----------------------------\n";
         }
-        return false;
     }
 
-    // DIAGONAL CHECK
-    bool checkDiagonal() const {
-        for (int i = 0; i <= rows - 4; ++i) {
-            for (int j = 0; j <= columns - 4; ++j) {
-                // Check diagonally (from top-left to bottom-right)
-                if (board[i][j] == currentPlayer &&
-                    board[i + 1][j + 1] == currentPlayer &&
-                    board[i + 2][j + 2] == currentPlayer &&
-                    board[i + 3][j + 3] == currentPlayer) {
-                    return true;
-                }
-                // Check diagonally (from top-right to bottom-left)
-                if (board[i][j + 3] == currentPlayer &&
-                    board[i + 1][j + 2] == currentPlayer &&
-                    board[i + 2][j + 1] == currentPlayer &&
-                    board[i + 3][j] == currentPlayer) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    // For Checking if the Game is Tied or not
+    // Check if the board is full
     bool isBoardFull() const {
         for (int i = 0; i < columns; ++i) {
             if (board[0][i] == ' ') {
@@ -132,61 +55,136 @@ public:
         return (choice == 'S' || choice == 's');
     }
 
-    void playGame() {
-        cout << "                ---------------------------------             \n";
-        cout << "                |                               |             \n";
-        cout << "                |     WELCOME TO CONNECT 4!     |             \n";
-        cout << "                |                               |             \n";
-        cout << "                ---------------------------------             \n";
-        cout << endl; 
-        cout << endl;
-        cout << "                ---------------------------------             \n";
-        cout << "                |                               |             \n";
-        cout << "                |             RULES             |             \n";
-        cout << "                |                               |             \n";
-        cout << "                ---------------------------------             \n";
-        cout << endl; 
-        cout << endl;
-        cout << " 1. Two-playe Red(R) & Blue(B) game." << endl;
-        cout << " 2. Board Size is 6 rows & 7 columns." << endl;
-        cout << " 3. A player wins, if he/she is able to connect 4 letters horizontally, vertically or diagonally. " << endl;
-        cout << " 4. Game Draw when matrix is fully filled." << endl;
-        cout << endl;
-        cout << endl;
-        cout << "                ---------------------------------             \n";
-        cout << "                |                               |             \n";
-        cout << "                |          HOW TO PLAY          |             \n";
-        cout << "                |                               |             \n";
-        cout << "                ---------------------------------             \n";
-        cout << endl; 
-        cout << endl;
-        cout << " 1. Game always starts with player- 1 i.e. Red(R)." << endl;
-        cout << " 2. In each step choose the column number in which you want to drop the letter." << endl;
-        cout << " 3. Rows will be filled from bottom to top in any column. " << endl;
-        cout << endl;
-        cout << "                ---------------------------------             \n";
-        cout << "                |                               |             \n";
-        cout << "                |          GAME STARTS          |             \n";
-        cout << "                |                               |             \n";
-        cout << "                ---------------------------------             \n";
-        cout << endl;
-      
-        while (!gameWon && !isBoardFull()) {
-            displayBoard();
-            cout << endl;
-            cout << "Player " << currentPlayer << ", enter the column number (0-6) : ";
-            int column;
-            cin >> column;
-            cout << endl;
+    // Check if a move is valid
+    virtual bool isValidMove(int column) const = 0;
 
-            if (isValidMove(column)) {
-                makeMove(column);
-                gameWon = checkWin();
-                switchPlayer();
-            } else {
-                cout << "Invalid move. Please try again.\n";
+    // Make a move
+    virtual void makeMove(int column, char currentPlayer) = 0;
+
+    // Check for a win
+    virtual bool checkWin(int row, int column, char currentPlayer) const = 0;
+};
+
+class Connect4Game : public Board {
+private:
+    char currentPlayer;
+    bool gameWon;
+    // Clear the input buffer
+    void clearInputBuffer() const {
+        cin.clear(); // clear input buffer to restore cin to a usable state
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ignore last input
+    }
+
+public:
+    Connect4Game() : currentPlayer('R'), gameWon(false) {}
+
+    // Switch Players
+    void switchPlayer() {
+        currentPlayer = (currentPlayer == 'R') ? 'B' : 'R';
+    }
+
+    // Input validation for a valid column number
+    bool isValidMove(int column) const override {
+        return column >= 0 && column < columns && board[0][column] == ' ';
+    }
+
+    // Make a move
+    void makeMove(int column, char currentPlayer) override {
+        for (int i = rows - 1; i >= 0; --i) {
+            if (board[i][column] == ' ') {
+                board[i][column] = currentPlayer;
+                break;
             }
         }
+    }
+
+    // Check for a win
+    bool checkWin(int row, int column, char currentPlayer) const override {
+        // Check horizontally, vertically, and diagonally for a win
+        return checkHorizontal(row, column) || checkVertical(row, column) || checkDiagonal(row, column);
+    }
+
+    // Check for a win horizontally
+    bool checkHorizontal(int row, int column) const {
+        for (int j = 0; j <= columns - connectWin; ++j) {
+            if (board[row][j] == currentPlayer &&
+                board[row][j + 1] == currentPlayer &&
+                board[row][j + 2] == currentPlayer &&
+                board[row][j + 3] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check for a win vertically
+    bool checkVertical(int row, int column) const {
+        for (int i = 0; i <= rows - connectWin; ++i) {
+            if (board[i][column] == currentPlayer &&
+                board[i + 1][column] == currentPlayer &&
+                board[i + 2][column] == currentPlayer &&
+                board[i + 3][column] == currentPlayer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check for a win diagonally
+    bool checkDiagonal(int row, int column) const {
+        for (int i = 0; i <= rows - connectWin; ++i) {
+            for (int j = 0; j <= columns - connectWin; ++j) {
+                // Check diagonally (from top-left to bottom-right)
+                if (board[i][j] == currentPlayer &&
+                    board[i + 1][j + 1] == currentPlayer &&
+                    board[i + 2][j + 2] == currentPlayer &&
+                    board[i + 3][j + 3] == currentPlayer) {
+                    return true;
+                }
+                // Check diagonally (from top-right to bottom-left)
+                if (board[i][j + connectWin - 1] == currentPlayer &&
+                    board[i + 1][j + connectWin - 2] == currentPlayer &&
+                    board[i + 2][j + connectWin - 3] == currentPlayer &&
+                    board[i + 3][j + connectWin - 4] == currentPlayer) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+      // Input validation with clearing the input buffer
+      int getValidColumn() const {
+          int column;
+          while (true) {
+              cout << "Player " << currentPlayer << ", enter the column number (0-6): ";
+              if (cin >> column) {
+                  // Input was successfully converted to an integer
+                  if (isValidMove(column)) {
+                      break;
+                  } else {
+                      cout << "Invalid move. Please try again.\n";
+                  }
+              } else {
+                  // Input was not an integer
+                  clearInputBuffer();
+                  cout << "Invalid input. Please enter a valid column number.\n";
+              }
+          }
+          return column;
+      }
+
+      // Start the game
+      void startGame() {
+          while (!gameWon && !isBoardFull()) {
+              displayBoard();
+              cout << endl;
+              int column = getValidColumn();
+
+              makeMove(column, currentPlayer);
+              gameWon = checkWin(rows - 1, column, currentPlayer);
+              switchPlayer();
+          }
 
         displayBoard();
         cout << endl;
@@ -201,21 +199,68 @@ public:
             gameWon = false;
             currentPlayer = 'R';
             board = vector<vector<char>>(rows, vector<char>(columns, ' '));
-            playGame();
+            startGame();
         } else {
-        cout << "                ---------------------------------             \n";
-        cout << "                |          GAME ENDS !!!        |             \n";
-        cout << "                |          THANK YOU !!!        |             \n";
-        cout << "                |       SEE YOU NEXT TIME !!!   |             \n";
-        cout << "                ---------------------------------             \n";
-        cout << endl;
+            cout << "---------------------------------\n";
+            cout << "|                               |\n";
+            cout << "|          GAME ENDS !!!        |\n";
+            cout << "|          THANK YOU !!!        |\n";
+            cout << "|       SEE YOU NEXT TIME !!!   |\n";
+            cout << "|                               |\n";
+            cout << "---------------------------------\n";
+            cout << endl;
+        }
     }
-  }
+};
+
+class GameManager : public Connect4Game {
+public:
+    void playGame() {
+      cout << "                ---------------------------------             \n";
+      cout << "                |                               |             \n";
+      cout << "                |     WELCOME TO CONNECT 4!     |             \n";
+      cout << "                |                               |             \n";
+      cout << "                ---------------------------------             \n";
+      cout << endl; 
+      cout << endl;
+      cout << "                ---------------------------------             \n";
+      cout << "                |                               |             \n";
+      cout << "                |             RULES             |             \n";
+      cout << "                |                               |             \n";
+      cout << "                ---------------------------------             \n";
+      cout << endl; 
+      cout << endl;
+      cout << " 1. Two-playe Red(R) & Blue(B) game." << endl;
+      cout << " 2. Board Size is 6 rows & 7 columns." << endl;
+      cout << " 3. A player wins, if he/she is able to connect 4 letters horizontally, vertically or diagonally. " << endl;
+      cout << " 4. Game Draw when matrix is fully filled." << endl;
+      cout << endl;
+      cout << endl;
+      cout << "                ---------------------------------             \n";
+      cout << "                |                               |             \n";
+      cout << "                |          HOW TO PLAY          |             \n";
+      cout << "                |                               |             \n";
+      cout << "                ---------------------------------             \n";
+      cout << endl; 
+      cout << endl;
+      cout << " 1. Game always starts with player- 1 i.e. Red(R)." << endl;
+      cout << " 2. In each step choose the column number in which you want to drop the letter." << endl;
+      cout << " 3. Rows will be filled from bottom to top in any column. " << endl;
+      cout << endl;
+      cout << "                ---------------------------------             \n";
+      cout << "                |                               |             \n";
+      cout << "                |          GAME STARTS          |             \n";
+      cout << "                |                               |             \n";
+      cout << "                ---------------------------------             \n";
+      cout << endl;
+
+        Connect4Game::startGame();
+    }
 };
 
 int main() {
-    Connect4Game game;
-    game.playGame();
+    GameManager manager;
+    manager.playGame();
 
     return 0;
 }
